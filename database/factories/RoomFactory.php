61960b56c138b7f368401e1884c797189446c854
sina.it91 <sina.it91@gmail.com>
2022-09-04 10:10:28 +0430

    Completion of test writing

--- database/factories/RoomFactory.php
+++ database/factories/RoomFactory.php
@@ -2,11 +2,13 @@
 
 namespace Database\Factories;
 
+use App\Models\Room;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class RoomFactory extends Factory
 {
+    protected $model = Room::class;
     /**
      * Define the model's default state.
      *
--- tests/Feature/Http/Controllers/ChatroomControllerTest.php
+++ tests/Feature/Http/Controllers/ChatroomControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Models\Room;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -10,8 +11,9 @@ use Tests\TestCase;
 
 class ChatroomControllerTest extends TestCase
 {
+    use WithFaker, RefreshDatabase;
     /**
-     * A basic feature test example.
+     * User login test
      *
      * @return void
      */
@@ -22,12 +24,60 @@ class ChatroomControllerTest extends TestCase
         $response->assertStatus(302);
     }
 
+    /**
+     * display room view success test
+     * @return void
+     */
     public function test_index_success()
     {
         $user = User::factory()->create();
         Auth::login($user);
-        $this->actingAs($user)->get(route('room.view'))->assertStatus(200)->assertViewIs('chatroom.room');
+        $this->actingAs($user)->get(route('room.view'))
+           ->assertStatus(200)
+           ->assertViewIs('chatroom.room');
+    }
 
-        $user->delete();
+    /**
+     * Room creation test
+     * @return void
+     */
+    public function test_create_room_success()
+    {
+        $title = $this->faker->sentence;
+        $user = User::factory()->create();
+        Auth::login($user);
+        $this->actingAs($user)->post(route('room.store'), [
+            'title'  => $title,
+        ])->assertStatus(302)->assertRedirect(route('room.view'));
+    }
+
+    /**
+     * Message display test
+     * @return void
+     */
+    public function test_message_view_success()
+    {
+        $user = User::factory()->has(Room::factory())->create();
+        $room = $user->rooms;
+        Auth::login($user);
+        $this->actingAs($user)->get(route('room.message.view', collect($room)->first()))
+            ->assertStatus(200)
+            ->assertViewIs('chatroom.message');
+    }
+
+    /**
+     * Message creation test
+     * @return void
+     */
+    public function test_message_store_success()
+    {
+        $user = User::factory()->has(Room::factory())->create();
+        $room = collect($user->rooms)->first();
+        Auth::login($user);
+        $this->actingAs($user)->post(route('room.message.store', $room), [
+            'message' => $this->faker->sentence,
+        ])
+            ->assertStatus(302)
+            ->assertRedirect(route('room.message.view',$room));
     }
 }
